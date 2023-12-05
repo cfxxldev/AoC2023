7ad82a2eb7a526dd960f7597ea68e9d01728f0d1
Andreas Gebert <47990291+cfxxldev@users.noreply.github.com>
2023-12-05 22:14:38 +0100

    Update buildall.sh

--- buildall.sh
+++ buildall.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 cd day1
-g++ --std=c++20 -O3 -ltbb day1.cpp && time bash -c 'cat ./input.txt | ./a.out'
+g++13 --std=c++20 -O3 -ltbb day1.cpp && time bash -c 'cat ./input.txt | ./a.out'
 cd ..
 
 cd day2
-g++ --std=c++20 -O3 -ltbb day2.cpp && time bash -c 'cat ./input.txt | ./a.out'
+g++13 --std=c++20 -O3 -ltbb day2.cpp && time bash -c 'cat ./input.txt | ./a.out'
 cd ..
 
 cd day3
-g++ --std=c++20 -O3 -ltbb day3.cpp && time bash -c 'cat ./input.txt | ./a.out'
+g++13 --std=c++20 -O3 -ltbb day3.cpp && time bash -c 'cat ./input.txt | ./a.out'
 cd ..
 
 cd day4
-g++ --std=c++20 -O3 -ltbb day4.cpp && time bash -c 'cat ./input.txt | ./a.out'
+g++13 --std=c++20 -O3 -ltbb day4.cpp && time bash -c 'cat ./input.txt | ./a.out'
 cd ..
 
 cd day5
-g++ --std=c++20 -O3 -ltbb day5.cpp && time bash -c 'cat ./input.txt | ./a.out'
+g++13 --std=c++20 -O3 -ltbb day5.cpp && time bash -c 'cat ./input.txt | ./a.out'
 cd ..
