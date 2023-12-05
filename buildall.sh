55e2188f3a60a155f325245691f16964c489dba0
Andreas Gebert <47990291+cfxxldev@users.noreply.github.com>
2023-12-05 21:52:36 +0100

    so much fun

--- buildall.sh
+++ buildall.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-pushd day1
+cd day1
 g++ --std=c++20 -O3 -ltbb day1.cpp && time bash -c 'cat ./input.txt | ./a.out'
-popd
+cd ..
 
-pushd day2
+cd day2
 g++ --std=c++20 -O3 -ltbb day2.cpp && time bash -c 'cat ./input.txt | ./a.out'
-popd
+cd ..
 
-pushd day3
+cd day3
 g++ --std=c++20 -O3 -ltbb day3.cpp && time bash -c 'cat ./input.txt | ./a.out'
-popd
+cd ..
 
-pushd day4
+cd day4
 g++ --std=c++20 -O3 -ltbb day4.cpp && time bash -c 'cat ./input.txt | ./a.out'
-popd
+cd ..
 
-pushd day5
+cd day5
 g++ --std=c++20 -O3 -ltbb day5.cpp && time bash -c 'cat ./input.txt | ./a.out'
-popd
+cd ..
