1fc0ccf941dad71e9675bd875accdaee00a16346
Andreas Gebert <47990291+cfxxldev@users.noreply.github.com>
2023-12-06 13:35:07 +0100

    blub

--- buildall.sh
+++ buildall.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/zsh
 
 cd day1
 cd ..
@@ -6,53 +6,53 @@ cd ..
 cd day1
 echo ""
 echo "gcc"
-gcc --std=c++20 -lstdc++ -ltbb -O3 day1.cpp && time bash -c 'cat input.txt | ./a.out'
+gcc --std=c++20 -lstdc++ -ltbb -O3 day1.cpp && time zsh -c 'cat input.txt | ./a.out'
 echo ""
 echo "clang"
-clang --std=c++20 -lstdc++ -ltbb -O3 day1.cpp && time bash -c 'cat input.txt | ./a.out'
+clang --std=c++20 -lstdc++ -ltbb -O3 day1.cpp && time zsh -c 'cat input.txt | ./a.out'
 cd ..
 
 cd day2
 echo ""
 echo "gcc"
-gcc --std=c++20 -lstdc++ -ltbb -O3 day2.cpp && time bash -c 'cat input.txt | ./a.out'
+gcc --std=c++20 -lstdc++ -ltbb -O3 day2.cpp && time zsh -c 'cat input.txt | ./a.out'
 echo ""
 echo "clang"
-clang --std=c++20 -lstdc++ -ltbb -O3 day2.cpp && time bash -c 'cat input.txt | ./a.out'
+clang --std=c++20 -lstdc++ -ltbb -O3 day2.cpp && time zsh -c 'cat input.txt | ./a.out'
 cd ..
 
 cd day3
 echo ""
 echo "gcc"
-gcc --std=c++20 -lstdc++ -ltbb -O3 day3.cpp && time bash -c 'cat input.txt | ./a.out'
+gcc --std=c++20 -lstdc++ -ltbb -O3 day3.cpp && time zsh -c 'cat input.txt | ./a.out'
 echo ""
 echo "clang"
-clang --std=c++20 -lstdc++ -ltbb -O3 day3.cpp && time bash -c 'cat input.txt | ./a.out'
+clang --std=c++20 -lstdc++ -ltbb -O3 day3.cpp && time zsh -c 'cat input.txt | ./a.out'
 cd ..
 
 cd day4
 echo ""
 echo "gcc"
-gcc --std=c++20 -lstdc++ -ltbb -O3 day4.cpp && time bash -c 'cat input.txt | ./a.out'
+gcc --std=c++20 -lstdc++ -ltbb -O3 day4.cpp && time zsh -c 'cat input.txt | ./a.out'
 echo ""
 echo "clang"
-clang --std=c++20 -lstdc++ -ltbb -O3 day4.cpp && time bash -c 'cat input.txt | ./a.out'
+clang --std=c++20 -lstdc++ -ltbb -O3 day4.cpp && time zsh -c 'cat input.txt | ./a.out'
 cd ..
 
 cd day5
 echo ""
 echo "gcc"
-gcc --std=c++20 -lstdc++ -ltbb -O3 day5.cpp && time bash -c 'cat input.txt | ./a.out'
+gcc --std=c++20 -lstdc++ -ltbb -O3 day5.cpp && time zsh -c 'cat input.txt | ./a.out'
 echo ""
 echo "clang"
-clang --std=c++20 -lstdc++ -ltbb -O3 day5.cpp && time bash -c 'cat input.txt | ./a.out'
+clang --std=c++20 -lstdc++ -ltbb -O3 day5.cpp && time zsh -c 'cat input.txt | ./a.out'
 cd ..
 
 cd day6
 echo ""
 echo "gcc"
-gcc --std=c++20 -lstdc++ -ltbb -O3 day6.cpp && time bash -c 'cat input.txt | ./a.out'
+gcc --std=c++20 -lstdc++ -ltbb -O3 day6.cpp && time zsh -c 'cat input.txt | ./a.out'
 echo ""
 echo "clang"
-clang --std=c++20 -lstdc++ -ltbb -O3 day6.cpp && time bash -c 'cat input.txt | ./a.out'
+clang --std=c++20 -lstdc++ -ltbb -O3 day6.cpp && time zsh -c 'cat input.txt | ./a.out'
 cd ..
